c247403a5cd3efa9cc70b41297d68ebf3d614148
Lawrence Murry <lawrencemurry@yahoo.com>
2016-04-20 15:49:28 -0500

    Added Paperclip to Gemfile

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass'
 gem 'devise', '~> 3.5.6'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 
 group :development, :test do
 	gem 'sqlite3'
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -18,11 +18,10 @@ class PinsController < ApplicationController
 
   def create
     @pin = current_user.pins.build(pin_params)
-
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render :'new'
+      render action:'new'
     end
   end
 
@@ -52,6 +51,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
   end
