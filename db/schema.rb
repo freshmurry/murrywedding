e3e25d993dc03f6aefac420269309215cf1b0a97
Lawrence Murry <lawrencemurry@yahoo.com>
2016-04-22 10:46:26 -0500

    updated schema file

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20160413231527) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string  "image_file_name"
+    t.string  "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
