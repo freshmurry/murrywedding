b88ccc721316ad6eef1862e48dbab2572612a0ff
Lawrence Murry <lawrencemurry@yahoo.com>
2016-04-23 10:26:39 -0500

    Add user id to pins migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   	#validates :password, :presence => true,
   				        		# :on => :create,
   				        		# :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }
-end
 
-class User <ActiveRecord::Base
   has_many :pins				        		
 end
