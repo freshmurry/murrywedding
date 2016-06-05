edb01ada57056a8b1ed5d37da29c26a947217d2b
Lawrence Murry <lawrencemurry@yahoo.com>
2016-06-05 16:27:48 -0500

    Merging update files from cloud 9 to gitbash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require masonry/jquery.masonry
+//= require masonry/jquery.imagesloaded.min
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
