40c79fb77bd766c2b7c2529056cb250a15240a4d
Lawrence Murry <lawrencemurry@yahoo.com>
2016-06-05 16:10:07 -0500

    Merged gemfile to gitbash

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425203291) do
+ActiveRecord::Schema.define(version: 20160425210329) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
