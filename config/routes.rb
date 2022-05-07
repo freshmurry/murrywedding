016eebf143df683e20aa77603b489c8e298b0479
Lawrence <lawrencemurry@yahoo.com>
2022-05-07 15:08:39 +0000

    Updated code

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resources :comments
   end  
 
-  devise_for :users	
+  devise_for :users,
+              path: '',
+              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'}  
  		
    get '/notifications' => 'notifications#index'
 
