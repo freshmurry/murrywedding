4ec2cb8b6105d0fd31dcdf11e4b925280d85524e
Lawrence <lawrencemurry@yahoo.com>
2022-05-22 20:13:03 +0000

    Updated Code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,16 @@
 Rails.application.configure do
+  # Sets Paperclip to upload images to Amazon S3
+  config.paperclip_defaults = {
+    :storage => :s3,
+    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :s3_region => ENV['AWS_REGION']
+    }
+  }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -95,15 +107,4 @@ Rails.application.configure do
       :domain         => 'murrywedding.herokuapp.com',
       :enable_starttls_auto => true
     }
-    
-  # Sets Paperclip to upload images to Amazon S3
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-    :s3_region => 'us-standard',
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
