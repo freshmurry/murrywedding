1522261f2559cd67b8375d6ea0aa3a2951428778
Lawrence Murry <lawrencemurry@yahoo.com>
2022-07-25 00:45:48 -0500

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
-require_relative 'application'
-# require File.expand_path('../application', __FILE__)
+# require_relative 'application'
+require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 # Paperclip config:
-Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
-Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
-Paperclip.options[:command_path] = "/usr/local/bin/"
+# Paperclip.options[:image_magick_path] = "/opt/ImageMagick/bin"
+# Paperclip.options[:command_path] = "/opt/ImageMagick/bin"
+# Paperclip.options[:command_path] = "/usr/local/bin/"
