df3181a025903b38e1d0609107fc59b302f2d49f
Lawrence Murry <lawrencemurry@yahoo.com>
2022-07-25 03:07:34 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     :storage => :s3,
     s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
     :s3_credentials => {
-      :bucket => ENV['BUCKET_NAME'],
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV['AWS_REGION']
