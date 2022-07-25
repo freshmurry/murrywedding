c5ee79bc786eee060e4f97755b459f7853a25667
Lawrence Murry <lawrencemurry@yahoo.com>
2022-07-25 03:10:54 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,24 +1,24 @@
 Rails.application.configure do
   # Sets Paperclip to upload images to Amazon S3
-    config.paperclip_defaults = {
+#     config.paperclip_defaults = {
+#     :storage => :s3,
+#     s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
+#     :s3_credentials => {
+#       :bucket => ENV['AWS_BUCKET'],
+#       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+#       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+#       :s3_region => ENV['AWS_REGION']
+#     }
+#   }
+  config.paperclip_defaults = {
     :storage => :s3,
-    s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com",
-    :s3_credentials => {
+    s3_credentials: {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :s3_region => ENV['AWS_REGION']
+      :region => ENV['AWS_REGION']
     }
   }
-#   config.paperclip_defaults = {
-#     storage: :s3,
-#     s3_credentials: {
-#       bucket: ENV['AWS_BUCKET'],
-#       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-#       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-#       region: ENV['AWS_REGION']
-#     }
-#   }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
