84d4aacd5428f7d2f8d62cdba09e2f803630baf1
Lawrence <lawrencemurry@yahoo.com>
2022-07-28 04:52:51 +0000

    Updated AWS S3 code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => ENV['AWS_REGION']
+      :s3_region => ENV['AWS_REGION']
     }
   }
 
