ed4a8c478554cce6f86e6da0bcc144456a14491d
Lawrence Murry <lawrencemurry@yahoo.com>
2022-08-27 00:46:05 -0500

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
-//= require toastr
 //= require bootstrap
 //= require jquery.turbolinks
 //= require turbolinks
+//= require masonry/jquery.masonry
 //= require_tree .
