8bd99bb15a068e6c913182b0ffb54775a6127b7a
Lawrence <lawrencemurry@yahoo.com>
2022-09-07 04:04:32 +0000

    Fixed comments db

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ group :development, :test do
 end
 
 group :production do
-	gem 'pg', '~> 0.20'
+	gem 'pg', '~> 0.20.0'
 	gem 'rails_12factor'
 end
 
--- db/migrate/20180706002628_create_comments.rb
+++ db/migrate/20180706002628_create_comments.rb
@@ -5,8 +5,6 @@ class CreateComments < ActiveRecord::Migration
       t.text :comment
 
       t.timestamps null: false
-      t.references :pin, foreign_key: true
-      t.references :user, foreign_key: true
     end
   end
 end
