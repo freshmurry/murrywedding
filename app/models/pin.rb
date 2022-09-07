f3deb095dc4e1b77eba693d15f5013943831c2fe
Lawrence Murry <lawrencemurry@yahoo.com>
2022-09-07 01:42:29 -0500

    Update pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,18 +4,9 @@ class Pin < ActiveRecord::Base
 	
 	# has_attached_file :image, style: { :medium => "300x300>", :thumb => "100x100>" }
 	
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 	validates :image, presence: true
 	validates :description, presence: true
 end
-
-# class Pin < ActiveRecord::Base
-# 	belongs_to :user
-
-# 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-
-# 	validates :image, presence: true
-# 	validates :description, presence: true
-# end
